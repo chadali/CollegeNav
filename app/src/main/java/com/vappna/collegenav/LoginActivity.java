c5e897269c738bdc96e1780619a82787c1c6463d
Varun-Nair <nairvarun18@gmail.com>
2015-11-14 01:02:32 -0500

    Started adding Login screen

--- app/src/main/java/com/vappna/collegenav/LoginActivity.java
+++ app/src/main/java/com/vappna/collegenav/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.vappna.collegenav;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,8 +15,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolbar);
+      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
+        Button login = (Button) findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, FindCollegeActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
